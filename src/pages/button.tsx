4adeb304a7f8ba79fac3cfbc0796fa6232fdc097
oryzaKD <oryza.kd@gmail.com>
2025-06-04 23:58:55 +0700

    change the info

--- src/pages/button.tsx
+++ src/pages/button.tsx
@@ -90,7 +90,7 @@ function Button() {
         <Task onClose={closeDialogTask} isLoadingTask={isLoadingTask}/>
       </Dialog>
 
-      {/* Actions Floating Button */}
+      {/* Actions Button */}
       <div className="quick-actions">
         {/* When no dialogs are open, show the toggle button and expandable menu */}
         {!showInboxDialog && !showTaskDialog && (
